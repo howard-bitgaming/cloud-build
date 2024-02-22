c36ff88d45edfcddaa951cd433bd120c2ab7159c
howard-bitgaming <157110201+howard-bitgaming@users.noreply.github.com>
2024-02-22 09:53:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,4 +11,23 @@ then
   GH_TOKEN=$GH_AUTH
 fi
 
-gcloud cloud-shell ssh --authorize-session --command=`curl -fsSL https://raw.githubusercontent.com/howard-bitgaming/cloud-build/main/cloud.sh`
+CMD_VAR="
+GH_TOKEN='$GH_TOKEN'
+HUB_PROJECT='$HUB_PROJECT'
+HUB_HOST='$HUB_HOST'
+HUB_FOLDER='$HUB_FOLDER'
+IMAGE_NAME='$IMAGE_NAME'
+OWNER='$OWNER'
+REPO='$REPO'
+BRANCH='$BRANCH'
+NODE_VERSION='$NODE_VERSION'
+BUILD_CMD='$BUILD_CMD'
+VERSION_SUFFIX='$VERSION_SUFFIX'
+DEPLOY_REPO='$DEPLOY_REPO'
+DEPLOY_OWNER='$DEPLOY_OWNER'
+DEPLOY_BRANCH='$DEPLOY_BRANCH'
+DEPLOY_FILE='$DEPLOY_FILE'
+DEPLOY_KEY='$DEPLOY_KEY'
+"
+
+gcloud cloud-shell ssh --authorize-session --command="${CMD_VAR}`curl -fsSL https://raw.githubusercontent.com/howard-bitgaming/cloud-build/main/cloud.sh`"
